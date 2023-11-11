aade3425bd98e8407a2c3bed3858714a0374e025
mirvasinikka <mirva_s@hotmail.com>
2023-11-11 12:15:15 +0200

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,29 @@
-import { useState } from 'react';
 
-import image1 from './assets/miri1.jpg';
-import image2 from './assets/miri4.jpg';
-import image3 from './assets/miri11.jpg';
 
 import CatTable from './components/CatTable';
 import CatForm from './components/CatForm';
 import CatAppBar from './components/CatAppBar';
-// import './App.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-// kovakoodattu esimerkkidata
-const mockData = [
+const router = createBrowserRouter([
   {
-    id: 1,
-    nimi: 'Miri',
-    laji: 'Scottish long hair',
-    sijainti: 'Helsinki',
-    omistaja: 'Mirva',
-    lelu: 'pallo',
-    kuva: image1,
+    element: <CatAppBar />,
+    errorElement: null,
+    children: [
+      {
+        path: '/',
+        element: <CatTable />,
+      },
+      {
+        path: 'add',
+        element: <CatForm />,
+      },
+    ],
   },
-  {
-    id: 2,
-    nimi: 'Musti',
-    laji: 'Persian',
-    sijainti: 'Espoo',
-    omistaja: 'Pekka',
-    lelu: 'naru',
-    kuva: image2,
-  },
-  {
-    id: 3,
-    nimi: 'Molla',
-    laji: 'Thai Siamese',
-    sijainti: 'Oulu',
-    omistaja: 'Vilma',
-    lelu: 'hiiri',
-    kuva: image3,
-  },
-];
+]);
 
 function App() {
-  const [cats, setCats] = useState(mockData);
-
-  const handleCatUpdate = (newCatInfo) => {
-    setCats([...cats, newCatInfo]);
-  };
-
-  return (
-    <>
-      <CatAppBar />
-      <CatTable cats={cats} />
-      <CatForm handleCatUpdate={handleCatUpdate} />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/components/CatTable.jsx
+++ src/components/CatTable.jsx
@@ -1,10 +1,56 @@
+import { useLocation } from 'react-router-dom';
 import './CatTable.css';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
+import image1 from '../assets/miri1.jpg';
+import image2 from '../assets/miri4.jpg';
+import image3 from '../assets/miri11.jpg';
 
-function CatTable({ cats }) {
+const mockData = [
+  {
+    id: 1,
+    nimi: 'Miri',
+    laji: 'Scottish long hair',
+    sijainti: 'Helsinki',
+    omistaja: 'Mirva',
+    lelu: 'pallo',
+    kuva: image1,
+  },
+  {
+    id: 2,
+    nimi: 'Musti',
+    laji: 'Persian',
+    sijainti: 'Espoo',
+    omistaja: 'Pekka',
+    lelu: 'naru',
+    kuva: image2,
+  },
+  {
+    id: 3,
+    nimi: 'Molla',
+    laji: 'Thai Siamese',
+    sijainti: 'Oulu',
+    omistaja: 'Vilma',
+    lelu: 'hiiri',
+    kuva: image3,
+  },
+];
+
+function CatTable() {
+  const location = useLocation();
+  const [cats, setCats] = useState(mockData);
+
   if (cats.length === 0) {
     return <p>Kissoja ei ole</p>;
   }
+
+
+  useEffect(() => {
+    if (location.state) {
+    setCats([...cats, location.state.cat]);
+    }
+  }, []);
 
   return (
     <table>
@@ -18,7 +64,7 @@ function CatTable({ cats }) {
         </tr>
       </thead>
       <tbody>
-        {cats.map(cat => {
+        {cats.map((cat) => {
           return (
             <tr key={cat.nimi + cat.lelu}>
               <td>
