24d4010e255f2d64be2fec897fedc4f28a882cb9
mirvasinikka <mirva_s@hotmail.com>
2023-11-11 17:03:37 +0200

    replace link state with appcontext

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,44 @@
-
-
 import CatTable from './components/CatTable';
 import CatForm from './components/CatForm';
 import CatAppBar from './components/CatAppBar';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { createContext, useState } from 'react';
+
+import image1 from './assets/miri1.jpg';
+import image2 from './assets/miri4.jpg';
+import image3 from './assets/miri11.jpg';
+
+const mockData = [
+  {
+    id: 1,
+    nimi: 'Miri',
+    laji: 'Scottish long hair',
+    sijainti: 'Helsinki',
+    omistaja: 'Mirva',
+    lelu: 'pallo',
+    kuva: image1,
+  },
+  {
+    id: 2,
+    nimi: 'Musti',
+    laji: 'Persian',
+    sijainti: 'Espoo',
+    omistaja: 'Pekka',
+    lelu: 'naru',
+    kuva: image2,
+  },
+  {
+    id: 3,
+    nimi: 'Molla',
+    laji: 'Thai Siamese',
+    sijainti: 'Oulu',
+    omistaja: 'Vilma',
+    lelu: 'hiiri',
+    kuva: image3,
+  },
+];
+
+export const AppContext = createContext(null);
 
 const router = createBrowserRouter([
   {
@@ -23,7 +58,13 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  const [cats, setCats] = useState(mockData);
+
+  return (
+    <AppContext.Provider value={{cats, setCats}}>
+      <RouterProvider router={router} />
+    </AppContext.Provider>
+  );
 }
 
 export default App;
