e7c37b0633913473055f5f5e92c82f1f7eb663a6
mirvasinikka <mirva_s@hotmail.com>
2023-12-06 19:49:09 +0200

    create cat crud

--- server.cjs
+++ server.cjs
@@ -12,9 +12,11 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(helmet({ crossOriginResourcePolicy: false }));
 
-const db = new sqlite3.Database('./users.db');
+const usersDB = new sqlite3.Database('./users.db');
+const catsDB = new sqlite3.Database('./cats.db');
 
-db.run(
+// Creating users
+usersDB.run(
   `
   CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -32,6 +34,28 @@ db.run(
   },
 );
 
+// Creating cats
+catsDB.run(
+  `
+  CREATE TABLE IF NOT EXISTS cats (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nimi TEXT,
+    laji TEXT,
+    sijainti TEXT,
+    omistaja TEXT,
+    lelu TEXT,
+    kuva TEXT
+  )
+`,
+  (err) => {
+    if (err) {
+      console.error('Error creating cats table: ', err.message);
+    } else {
+      console.log('Cats table created or already exists');
+    }
+  },
+);
+
 app.get('/api/data', (req, res) => {
   res.json({ message: 'hello, express!' });
 });
@@ -45,7 +69,7 @@ app.post('/api/register', async (req, res) => {
 
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  db.run('INSERT INTO users (username, email, password) VALUES (?, ?, ?)', [username, email, hashedPassword], (err) => {
+  usersDB.run('INSERT INTO users (username, email, password) VALUES (?, ?, ?)', [username, email, hashedPassword], (err) => {
     if (err) return res.status(400).json({ error: 'Username already taken or invalid input' });
   });
 
@@ -56,7 +80,7 @@ app.post('/api/register', async (req, res) => {
 app.post('/api/login', (req, res) => {
   const { username, password } = req.body;
 
-  db.get('SELECT * FROM users WHERE username = ?', [username], async (err, user) => {
+  usersDB.get('SELECT * FROM users WHERE username = ?', [username], async (err, user) => {
     if (err || !user) {
       return res.status(401).json({ error: 'Invalid username' });
     }
@@ -69,6 +93,63 @@ app.post('/api/login', (req, res) => {
     res.json({ message: 'Login successful' });
   });
 });
+
+app.post('/api/cats', (req, res) => {
+  const { nimi, laji, sijainti, omistaja, lelu, kuva } = req.body;
+
+  catsDB.run(
+    'INSERT INTO cats (nimi, laji, sijainti, omistaja, lelu, kuva) VALUES (?, ?, ?, ?, ?, ?)',
+    [nimi, laji, sijainti, omistaja, lelu, kuva],
+    (err) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({ message: 'Cat added successfully' });
+    },
+  );
+});
+
+app.get('/api/cats', (req, res) => {
+  catsDB.all('SELECT * FROM cats', [], (err, rows) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json(rows);
+  });
+});
+
+app.put('/api/cats/:id', (req, res) => {
+  const { id } = req.params;
+  const { nimi, laji, sijainti, omistaja, lelu, kuva } = req.body;
+
+  catsDB.run(
+    'UPDATE cats SET nimi = ?, laji = ?, sijainti = ?, omistaja = ?, lelu = ?, kuva = ? WHERE id = ?',
+    [nimi, laji, sijainti, omistaja, lelu, kuva, id],
+    (err) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({ message: 'Cat updated successfully' });
+    },
+  );
+});
+
+app.delete('/api/cats/:id', (req, res) => {
+  const { id } = req.params;
+
+  catsDB.run('DELETE FROM cats WHERE id = ?', [id], (err) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({ message: 'Cat deleted successfully' });
+  });
+});
+
+
 
 app.listen(port, () => {
   console.log(`server is runnig at http://localhost:${port}`);
