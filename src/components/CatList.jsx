9fb583f16ab0078d186be7f41e9b5df9265ce73b
mirvasinikka <mirva_s@hotmail.com>
2023-12-10 13:40:39 +0200

    reuse search in catlist

--- src/components/CatList.jsx
+++ src/components/CatList.jsx
@@ -1,43 +1,35 @@
 import { useLocation } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
-import { Box, Button, Grid, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, CircularProgress } from '@mui/material';
-import Pagination from '@mui/material/Pagination';
+import { Box, CircularProgress, Grid, Pagination } from '@mui/material';
 
 import CatCard from './CatCard';
+
+import CatSearch from './CatSearch';
 
 function CatList() {
   const [cats, setCats] = useState([]);
   const [openDialog, setOpenDialog] = useState(false);
   const location = useLocation();
   const [currentPage, setCurrentPage] = useState(1);
-  const [catsPerPage] = useState(10);
+  const [catsPerPage] = useState(12);
   const [totalPages, setTotalPages] = useState(0);
   const [isLoading, setLoading] = useState(true);
 
-  const deleteCat = async () => {
-    try {
-      const response = await fetch(`/api/cats/${catToDelete}`, { method: 'DELETE' });
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-
-      handleCloseDialog();
-    } catch (error) {
-      console.error('Failed to delete cat:', error);
-    }
-  };
+  const indexOfLastCat = currentPage * catsPerPage;
+  const indexOfFirstCat = indexOfLastCat - catsPerPage;
+  const currentCats = cats.slice(indexOfFirstCat, indexOfLastCat);
 
   useEffect(() => {
     const searchParams = new URLSearchParams(location.search);
     const city = searchParams.get('city');
-    const startDate = searchParams.get('startDate');
+    const startRentDate = searchParams.get('startDate');
     const endDate = searchParams.get('endDate');
 
     const fetchCats = async () => {
       const query = new URLSearchParams();
       if (city) query.append('city', city);
-      if (startDate) query.append('startDate', startDate);
+      if (startRentDate) query.append('startDate', startRentDate);
       if (endDate) query.append('endDate', endDate);
 
       try {
@@ -72,10 +64,6 @@ function CatList() {
     setCurrentPage(value);
   };
 
-  const indexOfLastCat = currentPage * catsPerPage;
-  const indexOfFirstCat = indexOfLastCat - catsPerPage;
-  const currentCats = cats.slice(indexOfFirstCat, indexOfLastCat);
-
   const handleLikes = async (catId) => {
     try {
       const response = await fetch(`/api/cats/like/${catId}`, {
@@ -105,28 +93,26 @@ function CatList() {
 
   return (
     <Box sx={{ marginTop: 3, width: '100%', padding: 2 }}>
-      <Grid container spacing={3}>
-        {currentCats.map((cat, index) => (
-          <Grid item key={index} xs={6} sm={6} md={4} lg={3}>
-            <CatCard cat={cat} handleLikes={handleLikes} />
+      <CatSearch compact />
+      {cats.length === 0 ? (
+        <div>
+          <p>Kissoja ei ole</p>
+        </div>
+      ) : (
+        <>
+          <Grid container spacing={3}>
+            {currentCats.map((cat, index) => (
+              <Grid item key={index} xs={6} sm={6} md={4} lg={3}>
+                <CatCard cat={cat} handleLikes={handleLikes} />
+              </Grid>
+            ))}
           </Grid>
-        ))}
-      </Grid>
-      <Dialog open={openDialog} onClose={handleCloseDialog} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
-        <DialogTitle id="alert-dialog-title">{'Confirm Delete'}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">Are you sure you want to delete this cat?</DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleCloseDialog}>Cancel</Button>
-          <Button onClick={deleteCat} color="error" autoFocus>
-            Confirm
-          </Button>
-        </DialogActions>
-      </Dialog>
-      <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: 3, marginBottom: 10 }}>
-        <Pagination count={totalPages} page={currentPage} onChange={handlePageChange} size="large" color="primary" />
-      </Box>
+
+          <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: 3, marginBottom: 10 }}>
+            <Pagination count={totalPages} page={currentPage} onChange={handlePageChange} size="large" color="primary" />
+          </Box>
+        </>
+      )}
     </Box>
   );
 }
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,71 +1,30 @@
-import { useState, useEffect } from 'react';
-import { Box, TextField, Button, Typography, Container, Grid } from '@mui/material';
-import { DatePicker } from '@mui/x-date-pickers';
-import Autocomplete from '@mui/material/Autocomplete';
-import { useNavigate } from 'react-router-dom';
-import cities from '../cities.json';
+import { Box, Typography, Container, Grid } from '@mui/material';
+import CatSearch from './CatSearch';
 
 const HomePage = () => {
-  const [city, setCity] = useState('');
-  const [startDate, setStartDate] = useState(null);
-  const [endDate, setEndDate] = useState(null);
-  const [suggestions, setSuggestions] = useState([]);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const cityNames = Object.values(cities);
-    setSuggestions(cityNames);
-  }, []);
-
-  const handleSearch = async () => {
-
-     const offset = startDate.getTimezoneOffset();
-     const adjustedStartDate = new Date(startDate.getTime() - offset * 60 * 1000);
-     const adjustedEndDate = new Date(endDate.getTime() - offset * 60 * 1000);
-
-    const formattedStartDate = adjustedStartDate.toISOString().split('T')[0];
-    const formattedEndDate = adjustedEndDate.toISOString().split('T')[0];
-   
-    const queryParams = new URLSearchParams({
-      city: city,
-      startDate: formattedStartDate,
-      endDate: formattedEndDate,
-    }).toString();
-
-    navigate(`/cats?${queryParams}`);
+  const backgroundStyle = {
+    backgroundImage: `url('/assets/background2.png')`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    height: '100vh',
+    width: '100vw',
   };
 
+  const color = '#f2f1f0';
+
   return (
-    <Container>
-      <Typography variant="h4" sx={{ marginTop: 4, marginBottom: 2 }}>
-        Rent a Cat
-      </Typography>
-      <Box component="form" noValidate autoComplete="off">
-        <Grid container spacing={2}>
-          <Grid item xs={12} sm={6}>
-            <Autocomplete
-              freeSolo
-              options={suggestions}
-              onInputChange={(_, newInputValue) => {
-                setCity(newInputValue);
-              }}
-              renderInput={(params) => <TextField {...params} label="City" fullWidth />}
-            />
-          </Grid>
-          <Grid item xs={12} sm={2}>
-            <DatePicker label="Start Date" inputFormat="dd.MM.yyyy" value={startDate} onChange={setStartDate} />
-          </Grid>
-          <Grid item xs={12} sm={2}>
-            <DatePicker label="End Date" inputFormat="dd.MM.yyyy" value={endDate} onChange={setEndDate} />
-          </Grid>
-          <Grid item xs={12}>
-            <Button variant="contained" color="primary" onClick={handleSearch}>
-              Search
-            </Button>
-          </Grid>
-        </Grid>
-      </Box>
-    </Container>
+    <Box sx={backgroundStyle}>
+      <Container sx={{ pt: 10 }}>
+        <Box sx={{ bgcolor: color, boxShadow: 3, borderRadius: '8px', overflow: 'hidden', width: 'auto', mx: 'auto', pb: 3, ml: 40, mr: 40 }}>
+          <Typography variant="h4" sx={{ marginTop: 4, marginBottom: 2, textAlign: 'center' }}>
+            Rent a Cat
+          </Typography>
+
+          <CatSearch sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: 2 }} />
+        </Box>
+      </Container>
+    </Box>
   );
 };
 
