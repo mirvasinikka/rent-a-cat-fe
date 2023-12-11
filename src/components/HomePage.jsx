c180f02ad687b7ee5b55fa4603baf084b4006256
mirvasinikka <mirva_s@hotmail.com>
2023-12-11 21:15:58 +0200

    fix home page css

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,10 @@
 import { Box, Typography, Container, Grid } from '@mui/material';
 import CatSearch from './CatSearch';
+import { useTheme } from '@emotion/react';
 
-const HomePage = () => {
+function HomePage() {
+  const theme = useTheme();
+
   return (
     <Box
       sx={{
@@ -14,23 +17,28 @@ const HomePage = () => {
       }}
     >
       <Container sx={{ pt: 10 }}>
-        <Grid container justifyContent="center" alignItems="center">
-          <Grid item>
-            <Box sx={{ bgcolor: '#f2f1f0', boxShadow: 3, borderRadius: '8px', overflow: 'hidden', width: 'auto', mx: 'auto', pb: 3 }}>
-              <Typography
-                color="secondary"
-                variant="h3"
-                sx={{ marginTop: 4, marginBottom: 2, textAlign: 'center', fontWeight: 900, textTransform: 'capitalize' }}
-              >
-                Rent a Cat 🐈
-              </Typography>
-              <CatSearch sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: 4 }} />
-            </Box>
-          </Grid>
-        </Grid>
+        <Box
+          sx={{
+            bgcolor: '#f2f1f0',
+            boxShadow: 3,
+            borderRadius: '8px',
+            overflow: 'hidden',
+            pb: 3,
+            [theme.breakpoints.up('md')]: {
+              ml: 40,
+              mr: 40,
+            },
+          }}
+        >
+          <Typography color="secondary" variant="h4" sx={{ marginTop: 4, marginBottom: 2, textAlign: 'center' }}>
+            Rent a Cat
+          </Typography>
+
+          <CatSearch sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: 2 }} />
+        </Box>
       </Container>
     </Box>
   );
-};
+}
 
 export default HomePage;
